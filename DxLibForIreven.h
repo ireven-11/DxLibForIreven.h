a4db2b5497ed40c72c84e5ddeccfc85f96cf0ed1
ireven <hensati11@gmail.com>
2025-11-01 18:40:31 +0900

    関数をいろいろ追加

--- DxLibForIreven.h
+++ DxLibForIreven.h
@@ -1,13 +1,14 @@
 #pragma once
 #include"DxLib.h"
 #include<math.h>
+#include<cassert>
 
 /// <summary>
 /// dxlib初期化
 /// </summary>
 /// <param name="screenWidht">スクリーン幅</param>
 /// <param name="screenHeight">スクリーン高さ</param>
-void InitDxLib(unsigned int screenWidht = 1920, unsigned int screenHeight = 1080)
+inline void InitDxLib(unsigned int screenWidht = 1920, unsigned int screenHeight = 1080)
 {
     SetGraphMode(screenWidht, screenHeight, 32);//ウィンドウのサイズとカラーモードを決める
     ChangeWindowMode(TRUE);						//ウィンドウモードにする
@@ -37,7 +38,7 @@ void InitDxLib(unsigned int screenWidht = 1920, unsigned int screenHeight = 1080
 /// <param name="movieHeght">動画高さ</param>
 /// <param name="moviePosition">動画座標</param>
 /// <param name="originScreenHandle">元々のスクリーンハンドル</param>
-bool PlayTransparentMovie(int movieHandle, int screenHandle, unsigned char movieBackColorType = 0, bool isLoop = true, int movieWidht = 1920, int movieHeight = 1080,
+inline bool PlayTransparentMovie(int movieHandle, int screenHandle, unsigned char movieBackColorType = 0, bool isLoop = true, int movieWidht = 1920, int movieHeight = 1080,
     VECTOR moviePosition = VGet(0.0f, 0.0f, 0.0f), int originScreenHandle = DX_SCREEN_BACK)
 {
     //スクリーンハンドルに動画を描画する
@@ -86,7 +87,7 @@ bool PlayTransparentMovie(int movieHandle, int screenHandle, unsigned char movie
 /// <param name="movieHandle">座標1</param>
 /// <param name="screenHandle">座標2</param>
 template<typename T>
-T CalculateDistance(VECTOR position1, VECTOR position2 = VGet(0.0f, 0.0f, 0.0f))
+inline T CalculateDistance(VECTOR position1, VECTOR position2 = VGet(0.0f, 0.0f, 0.0f))
 {
     //2点間の距離を計算する
     VECTOR tempVector = VGet(position1.x, position1.y, position1.z);
@@ -106,7 +107,7 @@ T CalculateDistance(VECTOR position1, VECTOR position2 = VGet(0.0f, 0.0f, 0.0f))
 /// <param name="tilSwitchTime">アニメーションが切り替わるまでの時間</param>
 /// <param name="finishGraphNumber">最後の画像がアニメーションの何番目かを表す数値</param>
 /// <param name="startGraphNumber">最初の画像がアニメーションの何番目かを表す数値</param>
-void DrawAnimationGraph(VECTOR position, int graphHandle[], int graphWidth, int graphHeight,
+inline void DrawAnimationGraph(VECTOR position, int graphHandle[], int graphWidth, int graphHeight,
     unsigned int tilSwitchTime, unsigned int finishGraphNumber, unsigned int startGraphNumber = 0)
 {
     //待ちカウントとアニメーションカウントを定義
@@ -148,7 +149,7 @@ void DrawAnimationGraph(VECTOR position, int graphHandle[], int graphWidth, int 
 /// <param name="color">文字の色</param>
 /// <param name="fontHandle">フォントハンドル</param>
 /// <param name="brinkSpeed">点滅スピード(デフォルトは2、0を入れると点滅しない)</param>
-void DrawBrinkStringToHandle(VECTOR position, const char* text, int color, int fontHandle = 0, unsigned short brinkSpeed = 2)
+inline void DrawBrinkStringToHandle(VECTOR position, const char* text, int color, int fontHandle = 0, unsigned short brinkSpeed = 2)
 {
     //点滅スピードが0だったら点滅しない
     if (brinkSpeed == 0)
@@ -182,7 +183,7 @@ void DrawBrinkStringToHandle(VECTOR position, const char* text, int color, int f
 /// <param name="graphHandle">画像反動</param>
 /// <param name="brinkSpeed">点滅スピード(デフォルトは2、0を入れると点滅しない)</param>
 /// <param name="isTrans">画像の透過処理を許すか</param>
-void DrawExtendBrinkGraph(VECTOR position, float widht, float height, int graphHandle, unsigned short brinkSpeed = 2, bool isTrans = true)
+inline void DrawExtendBrinkGraph(VECTOR position, float widht, float height, int graphHandle, unsigned short brinkSpeed = 2, bool isTrans = true)
 {
     //点滅スピードが0だったら点滅しない
     if (brinkSpeed == 0)
@@ -205,4 +206,209 @@ void DrawExtendBrinkGraph(VECTOR position, float widht, float height, int graphH
             DrawExtendGraph(position.x, position.y, position.x + widht, position.y + height, graphHandle, isTrans);
         }
     }
+}
+
+/// <summary>
+/// ベクトルの値を行列にセット
+/// </summary>
+/// <param name="matrix">値をセットする行列</param>
+/// <param name="vector">セットしたいベクトル</param>
+inline void SetMatrixFromVector(MATRIX& matrix, const VECTOR& vector)
+{
+    matrix.m[3][0] = vector.x;
+    matrix.m[3][1] = vector.y;
+    matrix.m[3][2] = vector.z;
+}
+
+/// <summary>
+/// コントローラー(DirectInput)の傾けてる角度を得る(返り値は0～3.14または0～-3.14)
+/// </summary>
+/// <param name="controllerNumber">どのコントローラから値を得るかの数値</param>
+/// <param name="deadZone">デッドゾーンの数値(0.0～1.0)</param>
+/// <returns></returns>
+inline double GetDInputStickTiltAngle(const int controllerNumber = DX_INPUT_PAD1, const float deadZone = 0.2f)
+{
+    //コントローラーのデッドゾーンを設定
+    SetJoypadDeadZone(controllerNumber, deadZone);
+
+    //移動方向を決定
+    DINPUT_JOYSTATE input;
+    //コントローラの入力を検知
+    GetJoypadDirectInputState(controllerNumber, &input);
+
+    //スティックの倒れてる数値から角度を求める
+    double tiltAngle_ = atan2(static_cast<double>(input.Y), static_cast<double>(input.X));
+
+    return tiltAngle_;
+}
+
+/// <summary>
+/// モデルボーン回転行列取得関数
+/// </summary>
+/// <param name="MHandle">モデルハンドル</param>
+/// <param name="FrameIndex">回転させたいボーン</param>
+/// <param name="Xaxis">x軸の回転させる値</param>
+/// <param name="Yaxis">y軸の回転させる値</param>
+/// <param name="Zaxis">z軸の回転させる値</param>
+inline MATRIX MV1GetFrameRotateMatrix(int MHandle, int FrameIndex, double Xaxis, double Yaxis, double Zaxis)
+{
+    auto frame_matrix       = MV1GetFrameLocalMatrix(MHandle, FrameIndex);
+    auto result_matrix      = MGetIdent();
+    const auto frame_pos    = MGetTranslateElem(frame_matrix);
+
+    CreateRotationZXYMatrix(&result_matrix, Xaxis, Yaxis, Zaxis);
+    SetMatrixFromVector(result_matrix, frame_pos);
+
+    return result_matrix;
+}
+
+/// <summary>
+/// 点が三角形ABCの内部にあるか（辺上も含む）
+/// </summary>
+/// <param name="A">三角形の座標１</param>
+/// <param name="B">三角形の座標２</param>
+/// <param name="C">三角形の座標３</param>
+/// <param name="P">判定したい座標</param>
+inline bool HitTriangleAndPixel(VECTOR A, VECTOR B, VECTOR C, VECTOR P)
+{
+    VECTOR AB = { B.x - A.x, 0, B.z - A.z };
+    VECTOR BC = { C.x - B.x, 0, C.z - B.z };
+    VECTOR CA = { A.x - C.x, 0, A.z - C.z };
+
+    VECTOR AP = { P.x - A.x, 0, P.z - A.z };
+    VECTOR BP = { P.x - B.x, 0, P.z - B.z };
+    VECTOR CP = { P.x - C.x, 0, P.z - C.z };
+
+    float c1 = Cross2D(AB, AP);
+    float c2 = Cross2D(BC, BP);
+    float c3 = Cross2D(CA, CP);
+
+    // すべて同じ符号なら内部（0は辺上）
+    if ((c1 >= 0 && c2 >= 0 && c3 >= 0) || (c1 <= 0 && c2 <= 0 && c3 <= 0)) {
+        return true;
+    }
+
+    return false;
+}
+
+/// <summary>
+/// 円が三角形ABCの内部にあるか（辺上も含む）
+/// </summary>
+/// <param name="trianglePos1">三角形の座標１</param>
+/// <param name="trianglePos2">三角形の座標２</param>
+/// <param name="trianglePos3">三角形の座標３</param>
+/// <param name="circlePos">判定したい円の座標</param>
+/// <param name="radius">判定したい円の半径</param>
+inline bool CheckHitCircleWithTriangle(VECTOR trianglePos1, VECTOR trianglePos2, VECTOR trianglePos3, VECTOR circlePos, float radius)
+{
+    VECTOR edge;				//線分
+    VECTOR edgeStartToCirclePos;//線分の始点から円の座標まで伸びるベクトル
+    VECTOR edgeEndToCirclePos;	//線分の終点から円の座標まで伸びるベクトル
+    VECTOR crossProduct;		//外積
+    float shortestDistance;		//線分との最短距離
+    for (auto i = 0; i < 3; i++)
+    {
+        switch (i)
+        {
+        case 0:
+            edge = VSub(trianglePos2, trianglePos1);
+            edgeStartToCirclePos = VSub(circlePos, trianglePos1);
+            edgeEndToCirclePos = VSub(circlePos, trianglePos2);
+            crossProduct = VCross(edge, edgeStartToCirclePos);
+            shortestDistance = CalculateDistance<float>(crossProduct) / CalculateDistance<float>(edge);
+            break;
+        case 1:
+            edge = VSub(trianglePos3, trianglePos2);
+            edgeStartToCirclePos = VSub(circlePos, trianglePos2);
+            edgeEndToCirclePos = VSub(circlePos, trianglePos3);
+            crossProduct = VCross(edge, edgeStartToCirclePos);
+            shortestDistance = CalculateDistance<float>(crossProduct) / CalculateDistance<float>(edge);
+            break;
+        case 2:
+            edge = VSub(trianglePos1, trianglePos3);
+            edgeStartToCirclePos = VSub(circlePos, trianglePos3);
+            edgeEndToCirclePos = VSub(circlePos, trianglePos1);
+            crossProduct = VCross(edge, edgeStartToCirclePos);
+            shortestDistance = CalculateDistance<float>(crossProduct) / CalculateDistance<float>(edge);
+            break;
+        }
+
+        //最短距離が半径より小さかったら当たってる可能性がある
+        if (shortestDistance <= radius)
+        {
+            //線分の外に円の中心があるときは角度が両方鈍角だったら当たってる
+            if (VDot(edge, edgeStartToCirclePos) * VDot(edge, edgeEndToCirclePos) <= 0)
+            {
+                return true;
+            }
+
+            //半径が円へのベクトルの距離より大きかったら当たってる
+            if (radius > CalculateDistance<float>(edgeStartToCirclePos) || radius > CalculateDistance<float>(edgeEndToCirclePos))
+            {
+                return true;
+            }
+        }
+
+        //三角形の中に円の中心点があるか
+        if (HitTriangleAndPixel(trianglePos1, trianglePos2, trianglePos3, circlePos))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/// <summary>
+/// 3d上に六角形を平面描画する関数
+/// </summary>
+/// <param name="standardPosition">六角形の基準となる座標</param>
+/// <param name="sideX">六角形のｘの長さ</param>
+/// <param name="sideZ">六角形のｚの長さ</param>
+/// <param name="sideZ2">六角形の何でいえばいいんだろ、こう、上と下の三角形の部分あるじゃん。そこまでのｚの長さっていえばいいのか？</param>
+/// <param name="color"></param>
+/// <param name="fill"></param>
+inline void DrawHexagon3D(VECTOR standardPosition, float sideX, float sideZ, float sideZ2, int color, bool fill)
+{
+    const float width_adjust = 2.0f;
+    const float height_adjust = 1.5f;
+    DrawTriangle3D(VGet(standardPosition.x - sideX / width_adjust, 0.0f, standardPosition.z + sideZ2 / height_adjust),
+        VGet(standardPosition.x + sideX / width_adjust, 0.0f, standardPosition.z + sideZ2 / height_adjust),
+        VGet(standardPosition.x, 0.0f, standardPosition.z + sideZ / height_adjust), color, fill);
+    DrawTriangle3D(VGet(standardPosition.x - sideX / width_adjust, 0.0f, standardPosition.z + sideZ2 / height_adjust),
+        VGet(standardPosition.x + sideX / width_adjust, 0.0f, standardPosition.z + sideZ2 / height_adjust),
+        VGet(standardPosition.x + sideX / width_adjust, 0.0f, standardPosition.z - sideZ2 / height_adjust), color, fill);
+    DrawTriangle3D(VGet(standardPosition.x - sideX / width_adjust, 0.0f, standardPosition.z + sideZ2 / height_adjust),
+        VGet(standardPosition.x - sideX / width_adjust, 0.0f, standardPosition.z - sideZ2 / height_adjust),
+        VGet(standardPosition.x + sideX / width_adjust, 0.0f, standardPosition.z - sideZ2 / height_adjust), color, fill);
+    DrawTriangle3D(VGet(standardPosition.x - sideX / width_adjust, 0.0f, standardPosition.z - sideZ2 / height_adjust),
+        VGet(standardPosition.x + sideX / width_adjust, 0.0f, standardPosition.z - sideZ2 / height_adjust),
+        VGet(standardPosition.x, 0.0f, standardPosition.z - sideZ / height_adjust), color, fill);
+}
+
+/// <summary>
+/// 2D用の外積を求める
+/// </summary>
+/// <param name="a">外積の計算に使う座標1</param>
+/// <param name="b">外積の計算に使う座標2</param>
+inline float Cross2D(VECTOR a, VECTOR b) 
+{
+    return a.x * b.z - a.z * b.x;
+}
+
+/// <summary>
+/// ベクトルをY軸回転する
+/// </summary>
+/// <param name="vector">回転させるベクトル</param>
+/// <param name="rotationAngle">回転角度</param>
+/// <returns></returns>
+inline VECTOR RotationVectorY(VECTOR vector, double rotationAngle)
+{
+    //角度法だとエラーが出るようにする
+    assert((-DX_PI_F < rotationAngle && rotationAngle < DX_PI_F) && "回転角度が弧度法（ラジアン）になってないです！これだとベクトルがよくわからんぐらい回転します！");
+
+    //回転
+    MATRIX rotationMatrix   = MGetRotY(rotationAngle);
+    vector                  = VTransform(vector, rotationMatrix);
+    return vector;
 }
